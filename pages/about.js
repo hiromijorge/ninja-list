399584f30b13f3b21cf55e5cdcdabad1cdfcf07e
hiromi <hiromi170117@gmail.com>
2021-07-19 14:24:20 +0700

    initial

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Laboris reprehenderit reprehenderit sit enim et tempor magna. Est duis
+          ut ut laboris excepteur fugiat enim laborum qui adipisicing cupidatat
+          in. Velit elit pariatur minim occaecat minim ut. Id dolore id Lorem
+          consequat laboris tempor laboris cillum consequat sit. Esse ut amet do
+          laboris ea nostrud cupidatat. Esse sint ea occaecat ex sint duis magna
+          veniam exercitation. Laborum id ea aliquip aliquip eiusmod commodo
+          commodo eiusmod nulla.
+        </p>
+        <p>
+          Laboris reprehenderit reprehenderit sit enim et tempor magna. Est duis
+          ut ut laboris excepteur fugiat enim laborum qui adipisicing cupidatat
+          in. Velit elit pariatur minim occaecat minim ut. Id dolore id Lorem
+          consequat laboris tempor laboris cillum consequat sit. Esse ut amet do
+          laboris ea nostrud cupidatat. Esse sint ea occaecat ex sint duis magna
+          veniam exercitation. Laborum id ea aliquip aliquip eiusmod commodo
+          commodo eiusmod nulla.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
